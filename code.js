a81240c6a398d7e4698829b8f85dc8c14380265a
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-05-30 09:36:39 -0500

    basic starting layout and basic timer functionality

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
-alert("first!!!");
+$(document).ready(function() {
+    // global variables
+    var totalSeconds = 100;
+    var interval;
+    var timeLeft = $('#time');
+
+    clearInterval(interval);
+    interval = setInterval( function() {
+        if (totalSeconds > 0) {
+            totalSeconds--;
+            timeLeft.text(totalSeconds);
+        } else {
+            // end game
+            return;
+        }
+    }, 100);
+});
+
+
+
