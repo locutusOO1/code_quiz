c30a971dcd47ddef7e2674c96b96130129bc83f9
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-05-30 23:52:26 -0500

    added game over screen with input functionality

--- code.js
+++ code.js
@@ -5,14 +5,16 @@ $(document).ready(function() {
     var interval;
     var timeLeft = $('#time');
     var questions = [["What is \"1\" + 1?",4,"5","2","\"11\"","\"1\""],
-                     ["What is the increment operator?",3,"-","++","+","%"],
-                     ["What is the decrement operator?",2,"--","-","/","^"],
-                     ["True/False: \"===\" always produces the same results as \"==\"",3,"True","False"],
-                     ["True/False: JavaScript allows you to create anonymous function (functions without a name)",2,"True","False"]];
+                    //  ["What is the increment operator?",3,"-","++","+","%"],
+                    //  ["What is the decrement operator?",2,"--","-","/","^"],
+                    //  ["True/False: \"===\" always produces the same results as \"==\"",3,"True","False"],
+                     ["True/False: JavaScript allows you to create anonymous functions (functions without a name)",2,"True","False"]];
     var quesIndex = -1;
     var result = "";
     var resClass = ""
     var lockAns = false;
+    var lockNav = false;
+    var initials = "";
     // content rows
     var headingRow = $('#heading_row');
     var contentRow = $('#content_row');
@@ -22,7 +24,7 @@ $(document).ready(function() {
     function initGame () {
         var newEl = $('<h3 id="main_header" class="mx-auto">Coding Quiz Challenge</h3>');
         newEl.appendTo(headingRow);
-        newEl = $('<h6 id="summary">This is a quiz that tests coding knowledge. The quiz is timed, and wrong answers deduct 5 seconds from the timer. Have fun!</h6>');
+        newEl = $('<h6 id="summary" class="mx-auto">This is a quiz that tests coding knowledge. The quiz is timed, and wrong answers deduct 5 seconds from the timer. Have fun!</h6>');
         newEl.appendTo(contentRow);
         newEl = $('<button id="begin" type="button" class="btn btn-primary mx-auto">Begin Quiz</button>');
         newEl.appendTo(buttonRow);
@@ -33,7 +35,9 @@ $(document).ready(function() {
 
     // View Scores
     function viewScores () {
-        alert("Score!");
+        if (!lockNav) {
+            alert("Score!");
+        }
     }
 
     // Go to Next Question
@@ -42,9 +46,9 @@ $(document).ready(function() {
         lockAns = false;
         quesIndex++;
         if (quesIndex < questions.length) {
-            var newEl = $('<h3 id="main_header">'+ questions[quesIndex][0] +'</h3>');
+            var newEl = $('<h3 id="main_header" class="mx-auto">'+ questions[quesIndex][0] +'</h3>');
             newEl.appendTo(headingRow);
-            newEl = $('<ul class="options"></ul>');
+            newEl = $('<ul class="options mx-auto"></ul>');
             for (var i = 2; i < questions[quesIndex].length; i++) {
                 var option = $('<li><button type="button" class="btn btn-primary" value="'+ i +'">'+ questions[quesIndex][i] +'</button></li>');
                 option.appendTo(newEl);
@@ -69,7 +73,7 @@ $(document).ready(function() {
                                 totalSeconds = 0;
                             }
                         }
-                        newEl = $('<h6 class="result '+ resClass +'">'+ result +'</h6>');
+                        newEl = $('<h6 class="result mx-auto text-center '+ resClass +'">'+ result +'</h6>');
                         newEl.appendTo(resultRow);
                         var timeOut = setTimeout(function() {
                             nextQues();
@@ -78,10 +82,11 @@ $(document).ready(function() {
                 }
             });
         } else {
-            alert("Game Over!!!");
+            gameOver();
         }
     }
 
+    // clear content
     function clearContent() {
         headingRow.empty();
         contentRow.empty();
@@ -89,7 +94,10 @@ $(document).ready(function() {
         resultRow.empty();
     }
 
+    // start game
     function startGame() {
+        // lock navigation
+        lockNav = true;
         // initialize timer
         totalSeconds = 100;
         clearInterval(interval);
@@ -104,12 +112,43 @@ $(document).ready(function() {
                 timeLeft.text(totalSeconds);
             } else {
                 // end game
-                return;
+                gameOver();
             }
         }, 1000);
     }
 
+    // show scores
+    showScores() {
+        alert("initials value: "+ initials + " score value: " + score);
+    }
+
     // end game when timer reaches 0
+    function gameOver() {
+        lockNav = false;
+        clearInterval(interval);
+        clearContent();
+        // alert("Game Over!!!\nScore: "+score);
+        var newEl = $('<h3 id="main_header" class="mx-auto">Game Over!!!</h3>');
+        newEl.appendTo(headingRow);
+        newEl = $('<h6 id="summary" class="mx-auto">Your final score is: ' + score + '</h6>');
+        newEl.appendTo(contentRow);
+        var newDiv = $('<div class="mx-auto"></div>');
+        newEl = $('<input type="text" class"form-control" id="initials" placeholder="Enter Initials Here"/>')
+        newEl.appendTo(newDiv);
+        newEl = $('<button id="sub_init" type="button" class="btn btn-primary">Submit</button>');
+        newEl.appendTo(newDiv);
+        newDiv.appendTo(buttonRow);
+        // submit button action listener
+        $('#sub_init').on("click",function(){
+            if ($('#initials').val()) {
+                initials = $('#initials').val();
+            } else {
+                initials = "Mysterious Stranger";
+            }
+            // show scores
+            showScores();
+        });
+    }
 
     // Start Game
     initGame();
